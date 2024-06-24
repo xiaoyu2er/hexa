79a251e41658447686021cf0a54ea7f869a9a8bb
yanqi.zong <yzong@turo.com>
2024-06-24 08:47:25 -0700

    fix: fix resend types

--- apps/web/src/app/api/email/route.ts
+++ apps/web/src/app/api/email/route.ts
@@ -5,6 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST() {
   try {
+    // @ts-ignore text is not required
     const { data, error } = await resend.emails.send({
       from: 'Hexa <noreply@hexa.im>',
       to: ['me@zyq.io'],
