af62231888c9b790da9b9f64ba7e9dedb947b21e
yanqi.zong <yzong@turo.com>
2024-06-24 08:37:27 -0700

    feat: add resend as dep, add api/email route

---
+++ apps/web/src/app/api/email/route.ts
@@ -0,0 +1,23 @@
+import { EmailTemplate } from '@/components/emails/hello';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Hexa <noreply@hexa.im>',
+      to: ['me@zyq.io'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
