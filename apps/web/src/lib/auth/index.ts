7ae0fa00393d7f6418b36cfc2feadc5e1156d3ad
yanqi.zong <yzong@turo.com>
2024-06-27 14:06:35 -0700

    feat: add lucia

---
+++ apps/web/src/lib/auth/index.ts
@@ -0,0 +1,32 @@
+import { Lucia, TimeSpan } from "lucia";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+import { db } from "@/db";
+import { sessionTable, userTable, type User as DbUser } from "@/db/schema";
+
+const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
+
+export const lucia = new Lucia(adapter, {
+    getSessionAttributes: (/* attributes */) => {
+        return {};
+    },
+    getUserAttributes: (attributes) => attributes,
+    sessionExpiresIn: new TimeSpan(30, "d"),
+    sessionCookie: {
+        name: "session",
+        expires: false, // session cookies have very long lifespan (2 years)
+        attributes: {
+            secure: process.env.NODE_ENV === "production",
+        },
+    },
+});
+
+
+declare module "lucia" {
+    interface Register {
+        Lucia: typeof lucia;
+        DatabaseSessionAttributes: DatabaseSessionAttributes;
+        DatabaseUserAttributes: DatabaseUserAttributes;
+    }
+    interface DatabaseSessionAttributes { }
+    interface DatabaseUserAttributes extends Omit<DbUser, "hashedPassword"> { }
+}
