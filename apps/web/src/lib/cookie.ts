7a324a1e2d6438495dcca59348851562b79384cb
yanqi.zong <yzong@turo.com>
2024-06-27 14:04:46 -0700

    feat: add util/cookie

---
+++ apps/web/src/lib/cookie.ts
@@ -0,0 +1,6 @@
+export function deleteCookie(sKey: string, sPath?: string, sDomain?: string) {
+    document.cookie = encodeURIComponent(sKey) +
+        "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" +
+        (sDomain ? "; domain=" + sDomain : "") +
+        (sPath ? "; path=" + sPath : "");
+}
