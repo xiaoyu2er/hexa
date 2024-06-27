f6f5f225d3e37c05f318f672569671c336a71574
yanqi.zong <yzong@turo.com>
2024-06-27 13:56:51 -0700

    chore: update ui build

--- packages/ui/tsup.config.ts
+++ packages/ui/tsup.config.ts
@@ -3,13 +3,14 @@ import { defineConfig, Options } from "tsup";
 export default defineConfig((options: Options) => ({
   entry: ["src/index.tsx"],
   format: ["esm"],
+  sourcemap: true,
   esbuildOptions(options) {
     options.banner = {
       js: '"use client"',
     };
   },
   dts: true,
-  minify: true,
+  minify: false,
   external: ["react"],
   ...options,
 }));
