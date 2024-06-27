e6379be4476c9d2ab7d15384095560751c70bde4
yanqi.zong <yzong@turo.com>
2024-06-27 14:08:18 -0700

    feat: add auth buttons

---
+++ apps/web/src/app/components/Logout.tsx
@@ -0,0 +1,16 @@
+
+"use client";
+
+import { logout } from "@/lib/auth/actions";
+import { Button } from "@hexa/ui";
+import { useServerAction } from "zsa-react";
+
+export const Logout = () => {
+
+    const { execute, isPending } = useServerAction(logout);
+
+    return <Button className="mt-4" onClick={() => execute({})} >
+        Log Out
+    </Button>
+
+}
