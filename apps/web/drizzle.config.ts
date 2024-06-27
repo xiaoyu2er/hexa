66e36ad121e07a71e153b8a241227503d899b922
yanqi.zong <yzong@turo.com>
2024-06-27 13:59:35 -0700

    feat: add drizzle

---
+++ apps/web/drizzle.config.ts
@@ -0,0 +1,13 @@
+import { config } from 'dotenv';
+import { defineConfig } from 'drizzle-kit';
+
+config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.local'});
+
+export default defineConfig({
+  schema: './src/db/schema.ts',
+  out: './drizzle/migrations',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DB_URL!,
+  },
+});
