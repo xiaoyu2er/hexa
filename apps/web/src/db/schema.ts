54953759e407aa4c1d6da70d8d1329d129f8dafc
yanqi.zong <yzong@turo.com>
2024-06-27 14:02:10 -0700

    feat: add auth schema

---
+++ apps/web/src/db/schema.ts
@@ -0,0 +1,75 @@
+import { boolean, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core"
+
+export const userTable = pgTable("user", {
+  id: text("id").primaryKey(),
+  name: text("name"),
+  email: text("email").unique(),
+  emailVerified: boolean("email_verified").notNull().default(false),
+  hashedPassword: text("hashed_password"),
+  avatarUrl: text("avatar_url"),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull().defaultNow(),
+  updatedAt: timestamp("updated_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull().defaultNow(),
+})
+
+export const sessionTable = pgTable("session", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull().defaultNow(),
+})
+
+export const emailVerificationTable = pgTable("email_verification", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  email: text("email").notNull(),
+  code: text("code").notNull(),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull().defaultNow(),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull(),
+})
+
+export const oauthAccountTable = pgTable("oauth_account", {
+  id: text("id").primaryKey(),
+  userId: text("user_id")
+    .notNull()
+    .references(() => userTable.id),
+  provider: text('provider', { enum: ["github", "google"] }).notNull(),
+  providerAccountId: text("provider_account_id").notNull(),
+  accessToken: text("access_token").notNull(),
+  refreshToken: text("refresh_token"),
+  expiresAt: timestamp("expires_at", {
+    withTimezone: true,
+    mode: "date",
+  }),
+  createdAt: timestamp("created_at", {
+    withTimezone: true,
+    mode: "date",
+  }).notNull().defaultNow(),
+})
+
+
+export type User = typeof userTable.$inferSelect;
+export type OAuthAccount = typeof oauthAccountTable.$inferSelect;
+export type Session = typeof sessionTable.$inferSelect;
+export type EmailVerification = typeof emailVerificationTable.$inferSelect;
