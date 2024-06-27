baec651c68a7d5975da176754d4744ef5b85b87b
yanqi.zong <yzong@turo.com>
2024-06-27 14:08:18 -0700

    feat: add auth buttons

---
+++ apps/web/src/app/components/Login.tsx
@@ -0,0 +1,11 @@
+
+"use client";
+
+import { Button } from "@hexa/ui";
+import Link from "next/link";
+
+export const Login = () => {
+    return <Button className="mt-4" asChild>
+        <Link href="/login">Login</Link>
+    </Button>
+}
