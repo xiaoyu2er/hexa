cf2149fd91b6e8417910c4087325c3b72bd7a991
yanqi.zong <yzong@turo.com>
2024-06-27 16:38:20 -0700

    feat: move emails to email-templates

---
+++ packages/email-templates/emails/components/Footer.tsx
@@ -0,0 +1,25 @@
+import { Hr, Tailwind, Text } from "@react-email/components";
+
+
+interface FooterProps {
+  email: string;
+}
+
+export default function Footer({
+  email
+}: FooterProps) {
+  return (
+    <Tailwind>
+      <Hr className="mx-0 my-6 w-full border border-gray-200" />
+      <Text className="text-[12px] leading-6 text-gray-500">
+        This email was intended for <span className="text-black">{email}</span>.
+        If you were not expecting this email, you can ignore this email.
+      </Text>
+    </Tailwind>
+  );
+}
+
+
+Footer.PreviewProps = {
+  email: "test@example.com"
+} as FooterProps;
