2de6947020d0f525e6d74baa11654cf7d0a20c9f
yanqi.zong <yzong@turo.com>
2024-06-28 16:48:34 -0700

    refactor: update verify email

--- apps/web/src/app/(auth)/verify-email/VerifyEmail.tsx
+++ apps/web/src/app/(auth)/verify-email/VerifyEmail.tsx
@@ -102,95 +102,88 @@ export const VerifyEmail: FC<VerifyEmailProps> = ({ email }) => {
   };
 
   return (
-    <div>
-      <Card className="w-full max-w-md">
-        <CardHeader className="text-center">
-          <CardTitle>{APP_TITLE} Verify Email</CardTitle>
+    <Card className="max-w-full w-96">
+      <CardHeader className="text-center">
+        <CardTitle>{APP_TITLE} Verify Email</CardTitle>
+        <CardDescription>
+          Please enter the verification code sent to your email
+        </CardDescription>
+        {email ? (
           <CardDescription>
-            Please enter the verification code sent to your email
+            <Link href="/sign-up" className="flex items-end justify-center">
+              {email}&nbsp;
+              <PencilLine className="h-4 w-4" aria-hidden="true" />
+            </Link>
           </CardDescription>
-          {email ? (
-            <CardDescription>
-              <Link href="/sign-up" className="flex items-end justify-center">
-                {email}&nbsp;
-                <PencilLine className="h-4 w-4" aria-hidden="true" />
-              </Link>
-            </CardDescription>
-          ) : null}
-        </CardHeader>
-        <CardContent>
-          <Form {...form}>
-            <form
-              onSubmit={handleSubmit(execVerify)}
-              ref={formRef}
-              method="POST"
-              className="space-y-4"
+        ) : null}
+      </CardHeader>
+      <CardContent>
+        <Form {...form}>
+          <form
+            onSubmit={handleSubmit(execVerify)}
+            ref={formRef}
+            method="POST"
+            className="space-y-4"
+          >
+            <FormField
+              control={form.control}
+              name="code"
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <InputOTP
+                      maxLength={VERIFY_CODE_LENGTH}
+                      autoFocus
+                      {...field}
+                      containerClassName="justify-center"
+                      onComplete={handleSubmit(execVerify)}
+                    >
+                      {[...Array(VERIFY_CODE_LENGTH).keys()].map((index) => (
+                        <InputOTPGroup key={index}>
+                          <InputOTPSlot
+                            index={index}
+                            className={
+                              errors.code || errors.root
+                                ? "border-destructive"
+                                : ""
+                            }
+                          />
+                        </InputOTPGroup>
+                      ))}
+                    </InputOTP>
+                  </FormControl>
+                  <FormMessage className="text-center" />
+                </FormItem>
+              )}
+            />
+            <p
+              className={cn(
+                "font-medium text-sm text-primary hover:underline hover:underline-offset-4 hover:cursor-pointer text-center",
+                {
+                  "opacity-70": count > 0,
+                },
+              )}
+              onClick={resed}
             >
-              <FormField
-                control={form.control}
-                name="code"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormControl>
-                      <InputOTP
-                        maxLength={VERIFY_CODE_LENGTH}
-                        autoFocus
-                        {...field}
-                        containerClassName="justify-center"
-                        onComplete={handleSubmit(execVerify)}
-                      >
-                        {[...Array(VERIFY_CODE_LENGTH).keys()].map((index) => (
-                          <InputOTPGroup key={index}>
-                            <InputOTPSlot
-                              index={index}
-                              className={
-                                errors.code || errors.root
-                                  ? "border-destructive"
-                                  : ""
-                              }
-                            />
-                          </InputOTPGroup>
-                        ))}
-                      </InputOTP>
-                    </FormControl>
-                    <FormMessage className="text-center" />
-                  </FormItem>
-                )}
-              />
-              <p
-                className={cn(
-                  "font-medium text-sm text-primary hover:underline hover:underline-offset-4 hover:cursor-pointer text-center",
-                  {
-                    "opacity-70": count > 0,
-                  },
-                )}
-                onClick={resed}
-              >
-                Didn't receive a code? Resend{" "}
-                {count > 0 ? `(${count}s)` : isRensedPending ? "..." : ""}
-              </p>
-              <LoadingButton
-                className="w-full"
-                loading={isSubmitting}
-                type="submit"
-              >
-                Verify
-              </LoadingButton>
-              <Button
-                variant="outline"
-                className="w-full"
-                type="button"
-                asChild
-              >
-                <Link href="/">Cancel</Link>
-              </Button>
-              <Button variant={"link"} size={"sm"} className="p-0" asChild>
-                <Link href={"/login"}>Have an account? Login</Link>
-              </Button>
-            </form>
-          </Form>
-        </CardContent>
-      </Card>
-    </div>
+              Didn't receive a code? Resend{" "}
+              {count > 0 ? `(${count}s)` : isRensedPending ? "..." : ""}
+            </p>
+            <LoadingButton
+              className="w-full"
+              loading={isSubmitting}
+              type="submit"
+            >
+              Verify
+            </LoadingButton>
+            <Button variant="outline" className="w-full" type="button" asChild>
+              <Link href="/">Cancel</Link>
+            </Button>
+            <Button variant={"link"} size={"sm"} className="p-0" asChild>
+              <Link href={"/login"}>Have an account? Login</Link>
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+    </Card>
   );
 };
