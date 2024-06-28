97d952cd8f79fa8d309d57fd3a0d7107779b6198
yanqi.zong <yzong@turo.com>
2024-06-28 11:59:35 -0700

    feat: update VerifyCode email tempalte

---
+++ packages/email-templates/emails/VerifyCode.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import {
+  Body,
+  Container,
+  Head,
+  Heading,
+  Hr,
+  Html,
+  Link,
+  Preview,
+  Section,
+  Tailwind,
+  Text,
+} from "@react-email/components";
+import Footer from "./components/Footer";
+
+interface VerifyCodeTemplateProps {
+  email: string;
+  code: string;
+}
+export default function VerifyCodeTemplate({
+  email,
+  code,
+}: VerifyCodeTemplateProps) {
+  return (
+    <Html>
+      <Head />
+      <Preview>{code} is your verification code</Preview>
+      <Tailwind>
+        <Body className="mx-auto my-auto bg-white font-sans">
+          <Container className="mx-auto my-10 max-w-[500px] rounded border border-solid border-gray-200 px-10 py-5">
+            <Heading className="mx-0 my-7 p-0 text-center text-xl font-semibold text-black">
+              Verify your code
+            </Heading>
+            <Text className="text-sm leading-6 text-black">
+              Welcome to Hexa.im!
+            </Text>
+
+            <Text className="text-sm leading-6 text-black">
+              We want to make sure it's really you. Please enter the following
+              verification code when prompted. You can ignore this message if you
+              didn't request a verification.
+            </Text>
+
+            <Text className="text-l font-semibold text-black text-center my-2">
+              Verification code
+            </Text>
+
+            <Section className="max-auto bg-slate-200">
+              <Text className="text-xl text-center">{code}</Text>
+            </Section>
+
+            <Text className="text-sm text-center mt-2 mb-4">
+              This code is valid for 10 minutes
+            </Text>
+
+            <Footer email={email} />
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
+
+VerifyCodeTemplate.PreviewProps = {
+  email: "test@example.com",
+  code: "123456",
+} as VerifyCodeTemplateProps;
