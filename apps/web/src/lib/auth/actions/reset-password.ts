5dfa6af8aeccb302ce10d96cde7b9c64fa1084cb
yanqi.zong <yzong@turo.com>
2024-06-30 16:11:19 -0700

    feat: update getHash imports

--- apps/web/src/lib/auth/actions/reset-password.ts
+++ apps/web/src/lib/auth/actions/reset-password.ts
@@ -13,12 +13,10 @@ import {
   sendVerificationCodeEmail,
   resendVerifyTokenEmail,
   verifyDBTokenByCode,
-  getHash,
 } from "./utils";
-import { lucia } from "../lucia";
-import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import { invalidateUserSessions, setSession } from "@/lib/session";
+import { getHash } from "@/lib/utils";
 
 export const forgetPasswordAction = createServerAction()
   .input(ForgetPasswordSchema)
@@ -33,7 +31,7 @@ export const forgetPasswordAction = createServerAction()
         "NOT_FOUND",
         process.env.NODE_ENV === "development"
           ? "Email not found"
-          : "Email not found",
+          : "Email not found"
       );
     }
 
@@ -57,7 +55,7 @@ export const resendResetPasswordCodeAction = createServerAction()
         "NOT_FOUND",
         process.env.NODE_ENV === "development"
           ? "Email not found"
-          : "Email not found",
+          : "Email not found"
       );
     }
 
@@ -77,7 +75,7 @@ export const verifyResetPasswordCodeAction = createServerAction()
         "NOT_FOUND",
         process.env.NODE_ENV === "development"
           ? "Email not found"
-          : "Email not found",
+          : "Email not found"
       );
     }
 
@@ -85,7 +83,7 @@ export const verifyResetPasswordCodeAction = createServerAction()
       user,
       { code },
       "RESET_PASSWORD",
-      false,
+      false
     );
     return { token: tokenRow.token };
   });
@@ -104,7 +102,7 @@ export const resetPasswordAction = createServerAction()
         "NOT_FOUND",
         process.env.NODE_ENV === "development"
           ? "Email not found"
-          : "Email not found",
+          : "Email not found"
       );
     }
 
