c65d8739f8c263e47550529f301b8bd9a664a8b0
yanqi.zong <yzong@turo.com>
2024-06-30 15:20:31 -0700

    feat: update schema

--- apps/web/drizzle/migrations/0002_calm_virginia_dare.sql
+++
@@ -1,4 +0,0 @@
-ALTER TYPE "tokenType" ADD VALUE 'RESET_PASSWORD';--> statement-breakpoint
-ALTER TYPE "tokenType" ADD VALUE 'VERIFY_EMAIL';--> statement-breakpoint
-ALTER TABLE "token" RENAME COLUMN "token_hash" TO "token";--> statement-breakpoint
-ALTER TABLE "token" ADD COLUMN "code" text NOT NULL;
