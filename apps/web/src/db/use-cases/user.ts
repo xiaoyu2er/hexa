cd664b4bfcac1d751db403d7c77ceb147ffc071a
yanqi.zong <yzong@turo.com>
2024-06-30 16:00:13 -0700

    feat: extract account & user data access utils

---
+++ apps/web/src/db/use-cases/user.ts
@@ -0,0 +1,41 @@
+import { GitHubUser, GoogleUser } from "@/types";
+import { createUser, getUserByEmail } from "../data-access/user";
+import {
+  createGithubAccount,
+  createGoogleAccount,
+} from "../data-access/account";
+
+export async function createUserByGithubAccount(githubUser: GitHubUser) {
+  let existingUser = await getUserByEmail(githubUser.email);
+
+  if (!existingUser) {
+    existingUser = await createUser({
+      email: githubUser.email,
+      emailVerified: true,
+    });
+  }
+
+  if (!existingUser) {
+    throw new Error("Failed to create user");
+  }
+  await createGithubAccount(existingUser.id, githubUser);
+  return existingUser;
+}
+
+export async function createUserByGoogleAccount(googleUser: GoogleUser) {
+  let existingUser = await getUserByEmail(googleUser.email);
+
+  if (!existingUser) {
+    existingUser = await createUser({
+      email: googleUser.email,
+      emailVerified: true,
+    });
+  }
+
+  if (!existingUser) {
+    throw new Error("Failed to create user");
+  }
+
+  await createGoogleAccount(existingUser.id, googleUser);
+  return existingUser;
+}
