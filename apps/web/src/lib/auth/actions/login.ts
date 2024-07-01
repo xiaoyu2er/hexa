e33f857f33cf83515c4c16ab7490046f3719c1fa
yanqi.zong <yzong@turo.com>
2024-06-30 23:00:09 -0700

    refactor: add turnstileProcedure

--- apps/web/src/lib/auth/actions/login.ts
+++ apps/web/src/lib/auth/actions/login.ts
@@ -7,34 +7,12 @@ import { createServerAction, ZSAError } from "zsa";
 import { setSession } from "@/lib/session";
 import { isHashValid } from "@/lib/utils";
 import { headers } from "next/headers";
+import { turnstileProcedure } from "./turnstile";
 
-export const loginAction = createServerAction()
+export const loginAction = turnstileProcedure
+  .createServerAction()
   .input(LoginSchema)
   .handler(async ({ input }) => {
-    const form = new URLSearchParams();
-    form.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY!);
-    form.append("response", input["cf-turnstile-response"]!);
-    form.append("remoteip", headers().get("x-forwarded-for") as string);
-
-    console.log("turnstile form", form.toString());
-
-    const result = await fetch(
-      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
-      { method: "POST", body: form },
-    );
-    const json = await result.json();
-    console.log("turnstile response", json);
-
-    if (json.success === false) {
-      throw new ZSAError(
-        "FORBIDDEN",
-        process.env.NODE_ENV === "development"
-          ? "Cloudflare Turnstile failed"
-          : // Human-readable error message
-            "Please complete the CAPTCHA",
-      );
-    }
-
     const { email, password } = input;
     const existingUser = await db.query.userTable.findFirst({
       where: (table, { eq }) => eq(table.email, email),
---
+++ apps/web/src/lib/auth/actions/turnstile.ts
@@ -0,0 +1,56 @@
+// https://developers.cloudflare.com/turnstile/get-started/server-side-validation/
+// Error code Description
+// missing-input-secret	The secret parameter was not passed.
+// invalid-input-secret	The secret parameter was invalid or did not exist.
+// missing-input-response	The response parameter (token) was not passed.
+// invalid-input-response	The response parameter (token) is invalid or has expired. Most of the time, this means a fake token has been used. If the error persists, contact customer support.
+// invalid-widget-id	The widget ID extracted from the parsed site secret key was invalid or did not exist.
+// invalid-parsed-secret	The secret extracted from the parsed site secret key was invalid.
+// bad-request	The request was rejected because it was malformed.
+// timeout-or-duplicate	The response parameter (token) has already been validated before. This means that the token was issued five minutes ago and is no longer valid, or it was already redeemed.
+// internal-error	An internal error happened while validating the response. The request can be retried.
+
+import { TurnstileSchema } from "@/lib/zod/schemas/auth";
+import { headers } from "next/headers";
+import { ZSAError, createServerActionProcedure } from "zsa";
+
+export interface TurnstileFailedValidationResponse {
+  success: boolean;
+  "error-codes": string[];
+}
+
+export const turnstileProcedure = createServerActionProcedure()
+  .input(TurnstileSchema)
+  .handler(async ({ input }) => {
+    const form = new URLSearchParams();
+    form.append("secret", process.env.CLOUDFLARE_TURNSTILE_SECRET_KEY!);
+    form.append("response", input["cf-turnstile-response"]!);
+    form.append("remoteip", headers().get("x-forwarded-for") as string);
+
+    console.log("turnstile form", form.toString());
+
+    const result = await fetch(
+      "https://challenges.cloudflare.com/turnstile/v0/siteverify",
+      { method: "POST", body: form },
+    );
+    const turnstileVerifyRes: TurnstileFailedValidationResponse =
+      await result.json();
+    // {
+    //     "success": false,
+    //     "error-codes": [
+    //       "invalid-input-response"
+    //     ]
+    //   }
+    console.log("turnstile response", turnstileVerifyRes);
+
+    if (turnstileVerifyRes.success === false) {
+      throw new ZSAError(
+        "FORBIDDEN",
+        process.env.NODE_ENV === "development"
+          ? "Cloudflare Turnstile failed - " +
+            turnstileVerifyRes["error-codes"][0]
+          : "Only humans are allowed to login. Please try again.",
+      );
+    }
+    return {};
+  });
