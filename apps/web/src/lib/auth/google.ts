e4720a3f13fba9a77bc7c90247b853113c24c542
yanqi.zong <yzong@turo.com>
2024-06-30 17:00:54 -0700

    feat: comment github and google clientId check

--- apps/web/src/lib/auth/google.ts
+++ apps/web/src/lib/auth/google.ts
@@ -1,12 +1,12 @@
 import { Google } from "arctic";
 import { PUBLIC_URL } from "@/lib/const";
 
-if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
-  throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET");
-}
+// if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+//   throw new Error("Missing GOOGLE_CLIENT_ID or GOOGLE_CLIENT_SECRET");
+// }
 
 export const google = new Google(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GOOGLE_CLIENT_ID!,
+  process.env.GOOGLE_CLIENT_SECRET!,
   `${PUBLIC_URL}/oauth/google/callback`,
 );
