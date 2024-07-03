31ac0dbaeed38b9310d9a988d35fbeb192cc42b4
yanqi.zong <yzong@turo.com>
2024-07-02 22:06:24 -0700

    feat: add update user avatar action

--- apps/web/src/lib/actions/user.ts
+++ apps/web/src/lib/actions/user.ts
@@ -1,9 +1,15 @@
 "use server";
 
 import { authenticatedProcedure } from "./procedures";
-import { UpdateUserNameSchema } from "../zod/schemas/user";
-import { updateUserName } from "../db/data-access/user";
+import {
+  UpdateAvatarSchema,
+  UpdateUserNameSchema,
+} from "@/lib/zod/schemas/user";
+import { updateUserAvatar, updateUserName } from "@/lib/db/data-access/user";
 import { revalidatePath } from "next/cache";
+import { isStored, storage } from "../storage";
+import { generateId } from "../utils";
+import { waitUntil } from "@vercel/functions";
 
 export const updateUserNameAction = authenticatedProcedure
   .createServerAction()
@@ -12,5 +18,23 @@ export const updateUserNameAction = authenticatedProcedure
     const { name } = input;
     const { user } = ctx;
     await updateUserName(user.id, name);
+    revalidatePath("/");
+  });
+
+export const updateUserAvatarAction = authenticatedProcedure
+  .createServerAction()
+  .input(UpdateAvatarSchema)
+  .handler(async ({ input, ctx }) => {
+    const { image } = input;
+    const { user } = ctx;
+    const { url } = await storage.upload(`avatars/${generateId()}`, image);
+    await updateUserAvatar(user.id, url);
+    waitUntil(
+      (async () => {
+        if (user.avatarUrl && isStored(user.avatarUrl)) {
+          await storage.delete(user.avatarUrl);
+        }
+      })(),
+    );
     revalidatePath("/");
   });
