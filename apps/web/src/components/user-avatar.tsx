732949dbd5f76d61414956a6e3d22453849026e5
yanqi.zong <yzong@turo.com>
2024-07-03 12:01:18 -0700

    refactor: extract user avatar

---
+++ apps/web/src/components/user-avatar.tsx
@@ -0,0 +1,63 @@
+import { logoutAction } from "@/lib/actions/logout";
+import { getAvatarFallbackUrl } from "@/lib/user";
+import { Avatar, AvatarImage, AvatarFallback } from "@hexa/ui/avatar";
+import { Button } from "@hexa/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@hexa/ui/dropdown-menu";
+import { Settings, LogOut } from "@hexa/ui/icons";
+import { User } from "lucia";
+import Link from "next/link";
+import { useServerAction } from "zsa-react";
+
+export function UserAvatar({ user }: { user: User }) {
+  const { execute: execLogout, isPending } = useServerAction(logoutAction);
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="secondary" size="icon" className="rounded-full">
+          <Avatar className="h-8 w-8">
+            <AvatarImage
+              src={user?.avatarUrl!}
+              alt={user?.name || "User Profile Picture"}
+            />
+            <AvatarFallback delayMs={200}>
+              <Avatar>
+                <AvatarImage
+                  src={getAvatarFallbackUrl(user)}
+                  alt={user?.name || "User Fallback Profile Picture"}
+                />
+              </Avatar>
+            </AvatarFallback>
+          </Avatar>
+          <span className="sr-only">Toggle user menu</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>My Account</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>
+          <Link href="/settings" className="flex gap-2 items-center">
+            <Settings className="h-4 w-4" /> Settings
+          </Link>
+        </DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem
+          disabled={isPending}
+          onClick={() => {
+            execLogout({});
+          }}
+          className="flex gap-2 items-center"
+        >
+          <LogOut className="w-4 h-4" /> Logout
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
