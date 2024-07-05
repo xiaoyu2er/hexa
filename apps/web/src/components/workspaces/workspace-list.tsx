d19e4a1eec5be305b300b178928ae0c36c0dba80
yanqi.zong <yzong@turo.com>
2024-07-05 07:20:08 -0700

    feat: update workspace list

--- apps/web/src/components/workspaces/workspace-list.tsx
+++ apps/web/src/components/workspaces/workspace-list.tsx
@@ -1,3 +1,30 @@
-export const WorkspaceList = () => {
-    return <>1</>
+"use server";
+
+import { validateRequest } from "@/lib/auth";
+import { getWorkspacesByUserId } from "@/lib/db/data-access/workspace";
+import NoWorkspaces from "./no-workspaces";
+import { WorkspaceCard } from "./workspace-card";
+
+export const WorkspaceList = async () => {
+  const { user } = await validateRequest();
+  if (!user) {
+    return null;
+  }
+
+  const list = await getWorkspacesByUserId(user.id);
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+  return (
+    <>
+      {list.length > 0 ? (
+        <div className="flex gap-5 flex-wrap">
+          {list.map((w) => {
+            return <WorkspaceCard key={w.id} workspace={w}></WorkspaceCard>;
+          })}
+        </div>
+      ) : (
+        <NoWorkspaces />
+      )}
+    </>
+  );
 };
