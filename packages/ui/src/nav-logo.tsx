0232060ef0e0ebd36cc75a2a5370d96e7a6db54b
yanqi.zong <yzong@turo.com>
2024-07-04 21:35:42 -0700

    feat: update settings-layout

--- packages/ui/src/nav-logo.tsx
+++ packages/ui/src/nav-logo.tsx
@@ -2,7 +2,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 export const NavLogo = () => {
   return (
-    <Avatar className="h-8 w-8">
+    <Avatar className="h-8 w-8 dark:border-gray-300 dark:border box-content">
       <AvatarImage
         src={"https://dubassets.com/avatars/cly4x47ur000ftlbgbn7ilmx7"}
         alt={"Hexa"}
