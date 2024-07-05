f34f431c07a8687eab9e3b4c942d1743c2745e90
yanqi.zong <yzong@turo.com>
2024-07-04 21:35:42 -0700

    feat: update settings-layout

--- apps/web/src/components/layouts/console-layout.tsx
+++ apps/web/src/components/layouts/console-layout.tsx
@@ -1,105 +1,29 @@
 "use client";
 
-import { useSession } from "@/providers/session-provider";
 import { ModeToggle } from "@/components/header/mode-toggle";
-import { Button } from "@hexa/ui/button";
-import { Menu, Package2, Search } from "@hexa/ui/icons";
-import { Input } from "@hexa/ui/input";
-import { Sheet, SheetTrigger, SheetContent } from "@hexa/ui/sheet";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import { ReactNode } from "react";
 import { UserAccountNav } from "@/components/user-account-nav";
-
-const CONSOLE_NAVBARS = [
-  {
-    name: "Settings",
-    href: "/settings",
-  },
-];
+import { MaxWidth } from "@hexa/ui/max-width";
+import { NavLogo } from "@hexa/ui/nav-logo";
 
 export function ConsoleLayout({ children }: { children: ReactNode }) {
-  const { user } = useSession();
-  const pathname = usePathname();
   return (
-    <div className="flex min-h-screen flex-col">
-      <header className="sticky top-0 flex h-16 items-center gap-4 border-b bg-background px-4 md:px-6">
-        <div className="container flex h-16 items-center justify-between py-4">
-          <nav className="hidden flex-col gap-6 text-lg font-medium md:flex md:flex-row md:items-center md:gap-5 md:text-sm lg:gap-6">
-            <Link
-              href="/"
-              className="flex items-center gap-2 text-lg font-semibold md:text-base"
-            >
-              <Package2 className="h-6 w-6" />
-              <span className="sr-only">Hexa</span>
-            </Link>
-
-            {CONSOLE_NAVBARS.map((navbar) => (
-              <Link
-                key={navbar.name}
-                href={navbar.href}
-                className={`${
-                  pathname.startsWith(navbar.href)
-                    ? "text-foreground"
-                    : "text-muted-foreground"
-                } transition-colors hover:text-foreground`}
-              >
-                {navbar.name}
+    <div className="min-h-screen w-full">
+      <header className="sticky top-0 flex h-16 items-center border-b bg-background">
+        <MaxWidth>
+          <div className="flex h-16 items-center justify-between">
+            <div className="flex items-center">
+              <Link href="/" className="hidden sm:block">
+                <NavLogo />
               </Link>
-            ))}
-          </nav>
-          <Sheet>
-            <SheetTrigger asChild>
-              <Button
-                variant="outline"
-                size="icon"
-                className="shrink-0 md:hidden"
-              >
-                <Menu className="h-5 w-5" />
-                <span className="sr-only">Toggle navigation menu</span>
-              </Button>
-            </SheetTrigger>
-            <SheetContent side="left">
-              <nav className="grid gap-6 text-lg font-medium">
-                <Link
-                  href="#"
-                  className="flex items-center gap-2 text-lg font-semibold"
-                >
-                  <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
-                </Link>
-
-                {CONSOLE_NAVBARS.map((navbar) => (
-                  <Link
-                    key={navbar.name}
-                    href={navbar.href}
-                    className={`${
-                      pathname === navbar.href
-                        ? "text-foreground"
-                        : "text-muted-foreground"
-                    } transition-colors hover:text-foreground`}
-                  >
-                    {navbar.name}
-                  </Link>
-                ))}
-              </nav>
-            </SheetContent>
-          </Sheet>
-          <div className="flex w-full items-center gap-4 md:ml-auto md:gap-2 lg:gap-4">
-            <form className="ml-auto flex-1 sm:flex-initial">
-              <div className="relative">
-                <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
-                <Input
-                  type="search"
-                  placeholder="Search products..."
-                  className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px]"
-                />
-              </div>
-            </form>
-            <ModeToggle />
-            <UserAccountNav user={user} />
+            </div>
+            <div className="flex items-center space-x-6">
+              <ModeToggle />
+              <UserAccountNav />
+            </div>
           </div>
-        </div>
+        </MaxWidth>
       </header>
       {children}
     </div>
--- apps/web/src/components/layouts/setting-layout.tsx
+++ apps/web/src/components/layouts/setting-layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { MaxWidth } from "@hexa/ui/max-width";
 import { cn } from "@hexa/utils";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -9,39 +10,42 @@ const SETTINGS_NAVBARS = [
     name: "General",
     href: "/settings",
   },
-  {
-    name: "Security",
-    href: "/settings/security",
-  },
 ];
 
 export function SettingsLayout({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
 
   return (
-    <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-      <div className="mx-auto grid w-full max-w-6xl gap-2">
-        <h1 className="text-3xl font-semibold">Settings</h1>
+    <main className="min-h-[calc(100vh-16px)]">
+      <div className="flex h-36 items-center border-b">
+        <MaxWidth>
+          <div className="flex items-center justify-between">
+            <h1 className="text-3xl font-semibold">Settings</h1>
+          </div>
+        </MaxWidth>
       </div>
-      <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-        <nav className="grid text-sm text-muted-foreground">
-          {SETTINGS_NAVBARS.map((nav) => (
-            <Link
-              key={nav.name}
-              href={nav.href}
-              className={cn(
-                "rounded-md p-2.5 text-sm transition-all duration-75 hover:bg-gray-100 active:bg-gray-200",
-                {
-                  "font-bold": nav.href === pathname,
-                }
-              )}
-            >
-              {nav.name}
-            </Link>
-          ))}
-        </nav>
-        <div className="grid gap-6">{children}</div>
-      </div>
+
+      <MaxWidth>
+        <div className="py-6 lg:py-10 grid max-w-6xl items-start gap-6 md:grid-cols-[120px_1fr] lg:grid-cols-[180px_1fr]">
+          <nav className="grid text-sm text-muted-foreground lg:sticky top-36">
+            {SETTINGS_NAVBARS.map((nav) => (
+              <Link
+                key={nav.name}
+                href={nav.href}
+                className={cn(
+                  "rounded-md p-2.5 text-md transition-all duration-75 hover:bg-gray-100 active:bg-gray-200",
+                  {
+                    "font-extrabold": nav.href === pathname,
+                  }
+                )}
+              >
+                {nav.name}
+              </Link>
+            ))}
+          </nav>
+          <div className="grid gap-6">{children}</div>
+        </div>
+      </MaxWidth>
     </main>
   );
 }
