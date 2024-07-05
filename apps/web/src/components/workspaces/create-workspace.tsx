a14a76b6c69756598fb17e3769eff9be6c4b5591
yanqi.zong <yzong@turo.com>
2024-07-05 13:06:11 -0700

    refactor: rename create ws modal

--- apps/web/src/components/workspaces/create-workspace.tsx
+++ apps/web/src/components/workspaces/create-workspace.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { Button } from "@hexa/ui/button";
-import { CreateWorkspaceModel } from "./create-workspace-modal";
+import { CreateWorkspaceModal } from "./create-workspace-modal";
 
 export function CreateWorkspace() {
   return (
-    <CreateWorkspaceModel>
+    <CreateWorkspaceModal>
       <Button>Create Workspace</Button>
-    </CreateWorkspaceModel>
+    </CreateWorkspaceModal>
   );
 }
