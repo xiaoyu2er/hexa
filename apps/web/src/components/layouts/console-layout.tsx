dbe40001dec1bd291fd709bd3e95602eed089284
yanqi.zong <yzong@turo.com>
2024-07-05 13:06:35 -0700

    feat: update console-layout

--- apps/web/src/components/layouts/console-layout.tsx
+++ apps/web/src/components/layouts/console-layout.tsx
@@ -1,22 +1,25 @@
-"use client";
-
 import { ModeToggle } from "@/components/header/mode-toggle";
 import Link from "next/link";
-import { ReactNode } from "react";
+import { ReactNode, Suspense } from "react";
 import { UserAccountNav } from "@/components/user-account-nav";
 import { MaxWidth } from "@hexa/ui/max-width";
 import { NavLogo } from "@hexa/ui/nav-logo";
+import { WorkspaceSwitcherServer } from "@/components/workspaces/workspace-switcher-server";
+import { Skeleton } from "@hexa/ui/skeleton";
 
 export function ConsoleLayout({ children }: { children: ReactNode }) {
   return (
     <div className="min-h-screen w-full">
       <header className="sticky top-0 flex h-16 items-center border-b bg-background">
         <MaxWidth>
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
+          <div className="flex h-16 items-center justify-between ">
+            <div className="flex items-center gap-4">
               <Link href="/" className="hidden sm:block">
                 <NavLogo />
               </Link>
+              <Suspense fallback={<Skeleton className="h-4 w-[250px]" />}>
+                <WorkspaceSwitcherServer />
+              </Suspense>
             </div>
             <div className="flex items-center space-x-6">
               <ModeToggle />
