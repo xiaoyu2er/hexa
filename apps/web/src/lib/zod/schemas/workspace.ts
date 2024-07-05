84c70c482ad413751a23d45ca80cbf3e646ede96
yanqi.zong <yzong@turo.com>
2024-07-05 07:18:18 -0700

    feat: add workspace zod schema

---
+++ apps/web/src/lib/zod/schemas/workspace.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const CreateWorkspaceSchema = z.object({
+  name: z
+    .string()
+    .min(1, "Please enter a name")
+    .max(32, "Name must be less than 32 characters"),
+  // only allow alphanumeric characters, dashes, and max length of 32
+  slug: z
+    .string()
+    .min(1, "Please enter a slug")
+    .max(32, "Slug must be less than 32 characters")
+    .regex(/^[a-zA-Z0-9-]+$/, "Slug must be alphanumeric and dashes only"),
+});
+
+export type CreateWorkspaceInput = z.infer<typeof CreateWorkspaceSchema>;
