73627924f9102395a6e385ff83f1ff337a8a9394
yanqi.zong <yzong@turo.com>
2024-07-05 13:05:51 -0700

    feat: add ws avatar

---
+++ apps/web/src/components/workspaces/workspace-avatar.tsx
@@ -0,0 +1,29 @@
+import { WorkspaceModel } from "@/lib/db";
+import { getAvatarFallbackUrl } from "@/lib/workspace";
+import { Avatar, AvatarImage, AvatarFallback } from "@hexa/ui/avatar";
+import { cn } from "@hexa/utils";
+
+export function WorkspaceAvatar({
+  workspace,
+  className,
+}: {
+  workspace: WorkspaceModel;
+  className: string;
+}) {
+  return (
+    <Avatar className={cn("h-6 w-6", className)}>
+      <AvatarImage
+        src={workspace?.avatarUrl!}
+        alt="Workspace Profile Picture"
+      />
+      <AvatarFallback delayMs={200}>
+        <Avatar>
+          <AvatarImage
+            src={getAvatarFallbackUrl(workspace)}
+            alt={workspace?.name || "WS"}
+          />
+        </Avatar>
+      </AvatarFallback>
+    </Avatar>
+  );
+}
