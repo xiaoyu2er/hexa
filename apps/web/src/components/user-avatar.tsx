00155bf00cef0ab81e65e6bf8c5217c303f61b3e
yanqi.zong <yzong@turo.com>
2024-07-06 22:17:38 -0700

    feat: update user-avatar

--- apps/web/src/components/user-avatar.tsx
+++ apps/web/src/components/user-avatar.tsx
@@ -1,4 +1,4 @@
-import { getAvatarFallbackUrl } from "@/lib/user";
+import { getAvatarFallbackName, getAvatarFallbackUrl } from "@/lib/user";
 import { Avatar, AvatarFallback, AvatarImage } from "@hexa/ui/avatar";
 import { cn } from "@hexa/utils";
 import { User } from "lucia";
@@ -13,16 +13,11 @@ export function UserAvatar({
   return (
     <Avatar className={cn("h-6 w-6", className)}>
       <AvatarImage
-        src={user?.avatarUrl!}
+        src={user?.avatarUrl || getAvatarFallbackUrl(user)}
         alt={user?.name || "User Profile Picture"}
       />
       <AvatarFallback delayMs={200}>
-        <Avatar>
-          <AvatarImage
-            src={getAvatarFallbackUrl(user)}
-            alt={user?.name || "User Fallback Profile Picture"}
-          />
-        </Avatar>
+        {getAvatarFallbackName(user)}
       </AvatarFallback>
     </Avatar>
   );
