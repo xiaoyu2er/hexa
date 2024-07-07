c3ab563163c929a26e881e201346b081a2ba6965
yanqi.zong <yzong@turo.com>
2024-07-06 17:50:20 -0700

    fix: warp reset-pasword in suspense

--- apps/web/src/app/(auth)/reset-password/page.tsx
+++ apps/web/src/app/(auth)/reset-password/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { ResetPassword } from "./reset-password";
 
 export const metadata = {
@@ -6,5 +7,9 @@ export const metadata = {
 };
 
 export default function () {
-  return <ResetPassword />;
+  return (
+    <Suspense>
+      <ResetPassword />
+    </Suspense>
+  );
 }
