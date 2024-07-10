82c72c8034f47eaef14180ad6b95eeffa58d28fb
yanqi.zong <yzong@turo.com>
2024-07-09 21:03:00 -0700

    fix: fix login actions

--- apps/web/src/lib/actions/login.ts
+++ apps/web/src/lib/actions/login.ts
@@ -6,44 +6,42 @@ import { ZSAError } from "zsa";
 import { setSession } from "@/lib/session";
 import { isHashValid } from "@/lib/utils";
 import { turnstileProcedure } from "./turnstile";
-import { getUserByEmail } from "../db/data-access/user";
+import { getUserEmail } from "../db/data-access/user";
 
 export const loginAction = turnstileProcedure
   .createServerAction()
   .input(LoginSchema)
   .handler(async ({ input }) => {
     const { email, password } = input;
-    const existingUser = await getUserByEmail(email);
+    const emailItem = await getUserEmail(email);
+    const existingUser = emailItem?.user;
 
     if (!existingUser) {
       throw new ZSAError(
         "FORBIDDEN",
         process.env.NODE_ENV === "development"
           ? "User does not exist"
-          : "Incorrect email or password",
+          : "Incorrect email or password"
       );
     }
 
-    if (!existingUser.hashedPassword) {
+    if (!existingUser.password) {
       throw new ZSAError(
         "FORBIDDEN",
         process.env.NODE_ENV === "development"
           ? "No password set"
-          : "Incorrect email or password",
+          : "Incorrect email or password"
       );
     }
 
-    const validPassword = await isHashValid(
-      existingUser.hashedPassword,
-      password,
-    );
+    const validPassword = await isHashValid(existingUser.password, password);
 
     if (!validPassword) {
       throw new ZSAError(
         "FORBIDDEN",
         process.env.NODE_ENV === "development"
           ? "Incorrect password"
-          : "Incorrect email or password",
+          : "Incorrect email or password"
       );
     }
 
