e4929cd5943061ab68bf851ec925a2043f9d8c7c
yanqi.zong <yzong@turo.com>
2024-07-09 21:05:26 -0700

    feat: add edit-user-email in user settings page

--- apps/web/src/app/(console)/settings/page.tsx
+++ apps/web/src/app/(console)/settings/page.tsx
@@ -2,6 +2,7 @@ import { DeleteAccount } from "@/components/user-settings/delete-account";
 import { EditName } from "@/components/user-settings/edit-name";
 import UploadAvatar from "@/components/user-settings/upload-avatar";
 import { UserId } from "@/components/user-settings/user-id";
+import { EditUserEmails } from "@/components/user-settings/edit-user-email";
 
 export const dynamic = "force-dynamic";
 
@@ -9,6 +10,7 @@ export default function () {
   return (
     <>
       <EditName />
+      <EditUserEmails />
       <UploadAvatar />
       <UserId />
       <DeleteAccount />
