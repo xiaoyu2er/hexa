18bff72b80eb7f1a9876a4161ed0f4705f3e5b8f
yanqi.zong <yzong@turo.com>
2024-07-09 21:04:49 -0700

    feat: add validate request in console layout

--- apps/web/src/app/(console)/layout.tsx
+++ apps/web/src/app/(console)/layout.tsx
@@ -1,6 +1,14 @@
 import { ConsoleLayout } from "@/components/layouts/console-layout";
+import { validateRequest } from "@/lib/auth";
+import { redirect } from "next/navigation";
 import type { ReactNode } from "react";
 
-export default function DashLayout({ children }: { children: ReactNode }) {
+export default async function DashLayout({
+  children,
+}: {
+  children: ReactNode;
+}) {
+  const { session } = await validateRequest();
+  if (!session) return redirect("/login");
   return <ConsoleLayout>{children}</ConsoleLayout>;
 }
