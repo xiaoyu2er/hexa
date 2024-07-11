e8676eaa64d16200b7fbd59d6946cc645eb10945
yanqi.zong <yzong@turo.com>
2024-07-10 17:16:56 -0700

    feat: add use-screen-size

---
+++ packages/ui/src/hooks/use-screen-size.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+
+export function useScreenSize() {
+  const [device, setDevice] = useState<"mobile" | "tablet" | "desktop" | null>(
+    null
+  );
+
+  useEffect(() => {
+    const checkDevice = () => {
+      if (window.matchMedia("(max-width: 640px)").matches) {
+        setDevice("mobile");
+      } else if (
+        window.matchMedia("(min-width: 641px) and (max-width: 1024px)").matches
+      ) {
+        setDevice("tablet");
+      } else {
+        setDevice("desktop");
+      }
+    };
+
+    checkDevice();
+
+    window.addEventListener("resize", checkDevice);
+
+    return () => {
+      window.removeEventListener("resize", checkDevice);
+    };
+  }, []);
+
+  return {
+    isMobile: device === "mobile",
+    isTablet: device === "tablet",
+    isDesktop: device === "desktop",
+  };
+}
