4382f973dcd7237957b47653dc88818ffc69fe77
yanqi.zong <yzong@turo.com>
2024-11-05 15:22:22 -0800

    fix: drizzle config db url

--- apps/web/drizzle.config.ts
+++ apps/web/drizzle.config.ts
@@ -6,13 +6,12 @@ config({
     process.env.NODE_ENV === "production" ? ".env.production" : ".env.local",
 });
 
+console.log("process.env.DB_LOCAL_PATH", process.env.DB_LOCAL_PATH);
 export default defineConfig({
   schema: "./src/lib/db/schema.ts",
   out: "./drizzle/migrations",
-  // driver: "d1",
   dialect: "sqlite",
   dbCredentials: {
-    // url: process.env.DB_URL ?? "",
-    url: ".wrangler/state/v3/d1/miniflare-D1DatabaseObject/3e2bb370d13f89dd177b80bc5184fd5470e46400063f799c2afca3e9cb993f6c.sqlite",
+    url: process.env.DB_LOCAL_PATH ?? "",
   },
 });
