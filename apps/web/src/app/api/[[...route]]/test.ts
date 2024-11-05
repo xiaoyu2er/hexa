24c9bf14c5216025ba056b8b43a68aedd77ef8bb
yanqi.zong <yzong@turo.com>
2024-11-05 15:19:41 -0800

    feat: add hono route

---
+++ apps/web/src/app/api/[[...route]]/test.ts
@@ -0,0 +1,22 @@
+import { DISABLE_CLOUDFLARE_TURNSTILE, PUBLIC_URL } from "@/lib/const";
+import { getCloudflareContext } from "@opennextjs/cloudflare";
+import { Hono } from "hono";
+
+const test = new Hono();
+
+test
+  .get("/hello", async (c) => {
+    const { env } = await getCloudflareContext();
+    return c.json({
+      message: env.hello,
+    });
+  })
+  .get("/env", async (c) => {
+    return c.json({
+      NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
+      PUBLIC_URL,
+      DISABLE_CLOUDFLARE_TURNSTILE,
+    });
+  });
+
+export default test;
