cf89e877595d6c1a4482a3aa272ea843d8784e9a
yanqi.zong <yzong@turo.com>
2024-11-26 17:03:40 -0800

    feat: redirect worker add info

--- apps/redirect/src/index.ts
+++ apps/redirect/src/index.ts
@@ -1,5 +1,29 @@
 export default {
-  async fetch(_request, _env, _ctx): Promise<Response> {
-    return new Response('Hello World!');
+  fetch(request, env, ctx): Promise<Response> {
+    const url = new URL(request.url);
+
+    return new Response(
+      JSON.stringify({
+        request,
+        ctx,
+        url: {
+          href: url.href,
+          origin: url.origin,
+          protocol: url.protocol,
+          host: url.host,
+          hostname: url.hostname,
+          pathname: url.pathname,
+          search: url.search,
+          searchParams: [...url.searchParams.entries()],
+        },
+        cf: request.cf,
+        env,
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
   },
 } satisfies ExportedHandler<Env>;
