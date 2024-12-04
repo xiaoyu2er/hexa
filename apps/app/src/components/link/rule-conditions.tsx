faa93e52fbed20f97ab5a12001068aa008e00b64
yanqi.zong <yzong@turo.com>
2024-12-03 21:07:07 -0800

    feat: add continent condition

--- apps/app/src/components/link/rule-conditions.tsx
+++ apps/app/src/components/link/rule-conditions.tsx
@@ -4,12 +4,17 @@ import { InputField } from '@/components/form/input-field';
 import { SelectField } from '@/components/form/select-field';
 import {
   RULE_FIELDS,
-  RULE_OPERATORS,
+  type RuleField,
+  type RuleOperator,
   type RulesFormType,
+  getFieldOperatorLabels,
+  getValueInputType,
+  getValueOptions,
 } from '@/server/schema/link';
 import { Button } from '@hexa/ui/button';
 import { TrashIcon } from '@hexa/ui/icons';
-import { useFieldArray, type useForm } from 'react-hook-form';
+import { MultiSelect } from '@hexa/ui/multi-select';
+import { type Path, useFieldArray, type useForm } from 'react-hook-form';
 
 export const RuleConditions = ({
   ruleIndex,
@@ -18,6 +23,7 @@ export const RuleConditions = ({
   ruleIndex: number;
   form: ReturnType<typeof useForm<RulesFormType>>;
 }) => {
+  const { watch } = form;
   const { fields, append, remove } = useFieldArray({
     control: form.control,
     name: `rules.${ruleIndex}.conditions`,
@@ -44,47 +50,104 @@ export const RuleConditions = ({
       </div>
 
       <div className="rounded-lg border p-3">
-        {fields.map((condition, condIndex) => (
-          <div key={condition.id}>
-            <div className="grid grid-cols-[1fr,1fr,1fr,auto] gap-2">
-              <SelectField
-                form={form}
-                name={`rules.${ruleIndex}.conditions.${condIndex}.field`}
-                options={RULE_FIELDS}
-                placeholder="Select field"
-              />
-              <SelectField
-                form={form}
-                name={`rules.${ruleIndex}.conditions.${condIndex}.operator`}
-                options={RULE_OPERATORS}
-                placeholder="Select operator"
-              />
-              <InputField
-                form={form}
-                name={`rules.${ruleIndex}.conditions.${condIndex}.value`}
-                placeholder="Enter value"
-              />
-              <Button
-                type="button"
-                variant="ghost"
-                size="icon"
-                onClick={() => remove(condIndex)}
-              >
-                <TrashIcon className="h-4 w-4" />
-              </Button>
-            </div>
+        {fields.map((condition, condIndex) => {
+          const conditionFieldName =
+            `rules.${ruleIndex}.conditions.${condIndex}.field` as Path<RulesFormType>;
+          const fieldValue = watch(conditionFieldName);
+          const conditionFieldOperatorLabels = getFieldOperatorLabels(
+            fieldValue as RuleField | undefined
+          );
+          const conditionOperatorName =
+            `rules.${ruleIndex}.conditions.${condIndex}.operator` as Path<RulesFormType>;
+          const operatorValue = watch(conditionOperatorName);
 
-            {condIndex < fields.length - 1 && (
-              <div className="my-2 flex items-center gap-2 px-2">
-                <div className="h-px flex-grow bg-border" />
-                <span className="font-medium text-muted-foreground text-xs">
-                  AND
-                </span>
-                <div className="h-px flex-grow bg-border" />
+          const valueInputType = getValueInputType(
+            fieldValue as RuleField | undefined,
+            operatorValue as RuleOperator | undefined
+          );
+
+          const valueOptions = getValueOptions(
+            fieldValue as RuleField | undefined,
+            operatorValue as RuleOperator | undefined
+          );
+
+          const input = (
+            <InputField
+              form={form}
+              name={`rules.${ruleIndex}.conditions.${condIndex}.value`}
+              placeholder="Enter value"
+            />
+          );
+
+          const multiSelect = (
+            <MultiSelect
+              options={valueOptions}
+              onValueChange={(values) => {
+                form.setValue(
+                  `rules.${ruleIndex}.conditions.${condIndex}.value`,
+                  values.length ? values : []
+                );
+              }}
+              placeholder="Select options..."
+              variant="default"
+              maxCount={3}
+              className="w-full"
+            />
+          );
+          const select = (
+            <SelectField
+              form={form}
+              name={`rules.${ruleIndex}.conditions.${condIndex}.value`}
+              options={valueOptions}
+              placeholder="Select value"
+            />
+          );
+
+          const valueInput =
+            valueInputType === 'text'
+              ? input
+              : // biome-ignore lint/nursery/noNestedTernary: <explanation>
+                valueInputType === 'multi-select'
+                ? multiSelect
+                : select;
+          return (
+            <div key={condition.id}>
+              <div className="grid grid-cols-[1fr,1fr,1fr,auto] gap-2">
+                <SelectField
+                  form={form}
+                  name={`rules.${ruleIndex}.conditions.${condIndex}.field`}
+                  options={RULE_FIELDS}
+                  placeholder="Select field"
+                />
+                <SelectField
+                  form={form}
+                  name={conditionOperatorName}
+                  options={conditionFieldOperatorLabels}
+                  placeholder="Select operator"
+                />
+                {valueInput}
+                <Button
+                  type="button"
+                  variant="ghost"
+                  size="icon"
+                  onClick={() => remove(condIndex)}
+                >
+                  <TrashIcon className="h-4 w-4" />
+                </Button>
               </div>
-            )}
-          </div>
-        ))}
+
+              {condIndex < fields.length - 1 && (
+                <div className="my-2 flex items-center gap-2 px-2">
+                  <div className="h-px flex-grow bg-border" />
+                  <span className="font-medium text-muted-foreground text-xs">
+                    AND
+                  </span>
+                  <div className="h-px flex-grow bg-border" />
+                </div>
+              )}
+            </div>
+          );
+        })}
 
         {fields.length === 0 && (
           <p className="text-center text-muted-foreground text-sm">
--- apps/app/src/server/schema/link.ts
+++ apps/app/src/server/schema/link.ts
@@ -9,7 +9,7 @@ import { z } from 'zod';
 // https://developers.cloudflare.com/ruleset-engine/rules-language/operators/#comparison-operators
 // https://openflagr.github.io/flagr/api_docs/#operation/createFlag
 
-export const zLinkRuleOperatorEnum = z.enum([
+export const zRuleOperatorEnum = z.enum([
   'EQ',
   'NEQ',
   'LT',
@@ -24,11 +24,11 @@ export const zLinkRuleOperatorEnum = z.enum([
   'NOT_IN',
 ]);
 
-export type LinkRuleOperator = z.infer<typeof zLinkRuleOperatorEnum>;
+export type RuleOperator = z.infer<typeof zRuleOperatorEnum>;
 
 export const LinkRuleConditionSchema = z.object({
   field: z.string(),
-  operator: zLinkRuleOperatorEnum,
+  operator: zRuleOperatorEnum,
   value: z.union([
     z.string(),
     z.number(),
@@ -164,21 +164,186 @@ export const LinkQuerySchema = PaginationSchema.merge(
 
 export type LinkQueryType = Simplify<z.infer<typeof LinkQuerySchema>>;
 
-export const RULE_OPERATORS: { label: string; value: LinkRuleOperator }[] = [
-  { label: 'Equals', value: 'EQ' },
-  { label: 'Not equals', value: 'NEQ' },
-  { label: 'Contains', value: 'CONTAINS' },
-  { label: 'Not contains', value: 'NOT_CONTAINS' },
-  { label: 'Matches regex', value: 'REG' },
-  { label: 'Not matches regex', value: 'NREG' },
+// 'EQ',
+// 'NEQ',
+// 'LT',
+// 'LE',
+// 'GT',
+// 'GE',
+// 'CONTAINS',
+// 'NOT_CONTAINS',
+// 'REG',
+// 'NREG',
+// 'IN',
+// 'NOT_IN',
+
+export const RULE_OPERATOR_LABEL_MAP: Record<
+  RuleOperator,
+  { label: string; value: RuleOperator }
+> = {
+  EQ: { label: 'Equals', value: 'EQ' },
+  NEQ: { label: 'Not equals', value: 'NEQ' },
+  LT: { label: 'Less than', value: 'LT' },
+  LE: { label: 'Less than or equal', value: 'LE' },
+  GT: { label: 'Greater than', value: 'GT' },
+  GE: { label: 'Greater than or equal', value: 'GE' },
+  CONTAINS: { label: 'Contains', value: 'CONTAINS' },
+  NOT_CONTAINS: { label: 'Not contains', value: 'NOT_CONTAINS' },
+  REG: { label: 'Matches regex', value: 'REG' },
+  NREG: { label: 'Not matches regex', value: 'NREG' },
+  IN: { label: 'In', value: 'IN' },
+  NOT_IN: { label: 'Not in', value: 'NOT_IN' },
+};
+
+export const getFieldOperatorLabels = (field?: RuleField) => {
+  if (!field) {
+    return [];
+  }
+  return RULE_FIELD_OPERATOR_MAP[field].map((operator) => {
+    return RULE_OPERATOR_LABEL_MAP[operator];
+  });
+};
+
+export const getValueInputType = (
+  field?: RuleField,
+  operator?: RuleOperator
+) => {
+  if (!field) {
+    return 'text';
+  }
+  if (!operator) {
+    return 'text';
+  }
+  switch (operator) {
+    case 'IN':
+    case 'NOT_IN':
+      return 'multi-select';
+    case 'EQ':
+    case 'NEQ':
+      return field === 'CONTINENT' ? 'select' : 'text';
+    default:
+      return 'text';
+  }
+};
+
+export const getValueOptions = (field?: RuleField, operator?: RuleOperator) => {
+  if (!field) {
+    return [];
+  }
+  if (!operator) {
+    return [];
+  }
+  if (field === 'CONTINENT') {
+    switch (operator) {
+      case 'IN':
+      case 'NOT_IN':
+      case 'EQ':
+      case 'NEQ':
+        return getContinentLabels();
+      default:
+        return [];
+    }
+  }
+  return [];
+};
+
+export const getContinentLabels = () => {
+  return [
+    { label: 'North America', value: 'NA' },
+    { label: 'South America', value: 'SA' },
+    { label: 'Europe', value: 'EU' },
+    { label: 'Africa', value: 'AF' },
+    { label: 'Asia', value: 'AS' },
+    { label: 'Oceania', value: 'OC' },
+    { label: 'Antarctica', value: 'AN' },
+  ];
+};
+
+export const RULE_OPERATORS: { label: string; value: RuleOperator }[] = [
+  ...Object.values(RULE_OPERATOR_LABEL_MAP),
 ];
 
-export const RULE_FIELDS = [
-  { label: 'Country', value: 'country' },
-  { label: 'Device', value: 'device' },
-  { label: 'Browser', value: 'browser' },
-  { label: 'Language', value: 'language' },
-  { label: 'Query param', value: 'query' },
+export const zRuleFieldEnum = z.enum([
+  // ===== Request Headers =====
+  'REFERER',
+  'IP',
+  'SOURCE',
+  'LANGUAGE',
+  'QUERY',
+  'SOURCE',
+
+  // ====== User agent ======
+  'USER_AGENT',
+  // Browser
+  'BROWSER_NAME',
+  'BROWSER_VERSION',
+  'BROWSER_MAJOR',
+  'BROWSER_TYPE',
+  // Device
+  'DEVICE_TYPE',
+  'DEVICE_VENDOR',
+  'DEVICE_MODEL',
+  // OS
+  'OS_NAME',
+  'OS_VERSION',
+  // CPU
+  'CPU',
+  // ======== Location ========
+  'CONTINENT',
+  'COUNTRY',
+  'IS_EU_COUNTRY',
+  'REGION_CODE',
+  'LATITUDE',
+  'LONGITUDE',
+  'TIMEZONE',
+  'POSTAL_CODE',
+]);
+
+export type RuleField = z.infer<typeof zRuleFieldEnum>;
+
+export const RULE_FIELD_OPERATOR_MAP: Record<RuleField, RuleOperator[]> = {
+  REFERER: ['CONTAINS', 'NOT_CONTAINS', 'REG', 'NREG'],
+  IP: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  SOURCE: ['CONTAINS', 'NOT_CONTAINS', 'REG', 'NREG'],
+  LANGUAGE: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  QUERY: ['CONTAINS', 'NOT_CONTAINS', 'REG', 'NREG'],
+  USER_AGENT: ['CONTAINS', 'NOT_CONTAINS', 'REG', 'NREG'],
+  BROWSER_NAME: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  BROWSER_VERSION: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  BROWSER_MAJOR: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  BROWSER_TYPE: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  DEVICE_TYPE: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  DEVICE_VENDOR: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  DEVICE_MODEL: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  OS_NAME: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  OS_VERSION: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  CPU: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  CONTINENT: ['IN', 'NOT_IN', 'EQ', 'NEQ'],
+  COUNTRY: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  IS_EU_COUNTRY: ['EQ', 'NEQ'],
+  REGION_CODE: ['EQ', 'NEQ'],
+  LATITUDE: ['EQ', 'NEQ', 'LT', 'LE', 'GT', 'GE'],
+  LONGITUDE: ['EQ', 'NEQ', 'LT', 'LE', 'GT', 'GE'],
+  TIMEZONE: ['EQ', 'NEQ', 'IN', 'NOT_IN'],
+  POSTAL_CODE: [
+    'EQ',
+    'NEQ',
+    'IN',
+    'NOT_IN',
+    'CONTAINS',
+    'NOT_CONTAINS',
+    'REG',
+    'NREG',
+  ],
+};
+
+export const RULE_FIELDS: { label: string; value: RuleField }[] = [
+  // { label: 'Country', value: 'COUNTRY' },
+  { label: 'Continent', value: 'CONTINENT' },
+  // { label: 'Device', value: 'DEVICE_TYPE' },
+  // { label: 'Browser', value: 'BROWSER_NAME' },
+  // { label: 'Language', value: 'LANGUAGE' },
+  // { label: 'Query param', value: 'QUERY' },
 ];
 
 export const RulesSchema = z.object({
