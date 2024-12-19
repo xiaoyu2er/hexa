15206aa1619e22a1cb4350e44282d3d4212277a2
yanqi.zong <yzong@turo.com>
2024-12-19 15:24:03 -0800

    feat: update check-route

--- apps/app/src/lib/check-route-permission.ts
+++ apps/app/src/lib/check-route-permission.ts
@@ -2,6 +2,11 @@ import { headers } from 'next/headers';
 import { notFound } from 'next/navigation';
 import { APP_HOST } from './env';
 
+export const getHost = async () => {
+  const header = await headers();
+  return header.get('host');
+};
+
 export const isAppHost = (host: string) => {
   return host === APP_HOST;
   // return true;
@@ -11,8 +16,7 @@ export const isAppHost = (host: string) => {
  * We don't allow custom domains to access routes other than /[slug].
  */
 export async function protectRoute() {
-  const header = await headers();
-  const host = header.get('host');
+  const host = await getHost();
   if (!host || !isAppHost(host)) {
     return notFound();
   }
