206d7895664b4ab4c2c1eac8b34ab1b01b404f26
yanqi.zong <yzong@turo.com>
2024-12-27 10:52:36 -0800

    feat: add drizzle studio for prod and staging env

--- apps/app/drizzle.config.ts
+++ apps/app/drizzle.config.ts
@@ -2,15 +2,38 @@ import { config } from 'dotenv';
 import { defineConfig } from 'drizzle-kit';
 
 config({
-  path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env',
+  path:
+    process.env.NODE_ENV === 'production'
+      ? '.env.production'
+      : // biome-ignore lint/nursery/noNestedTernary: <explanation>
+        // @ts-ignore
+        process.env.NODE_ENV === 'staging'
+        ? '.env.staging'
+        : '.env',
 });
 
-export default defineConfig({
-  schema: './src/schema.ts',
-  out: './drizzle/migrations',
-  dialect: 'sqlite',
-  // driver: 'd1-http',
-  dbCredentials: {
-    url: process.env.DB_LOCAL_PATH ?? '',
-  },
-});
+const isLocal = process.env.DB_LOCAL_PATH !== undefined;
+
+const drizzle = isLocal
+  ? defineConfig({
+      schema: './src/schema.ts',
+      out: './drizzle/migrations',
+      dialect: 'sqlite',
+      // driver: 'd1-http',
+      dbCredentials: {
+        url: process.env.DB_LOCAL_PATH ?? '',
+      },
+    })
+  : defineConfig({
+      schema: './src/schema.ts',
+      out: './drizzle/migrations',
+      dialect: 'sqlite',
+      driver: 'd1-http',
+      dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? '',
+        databaseId: process.env.CLOUDFLARE_DATABASE_ID ?? '',
+        token: process.env.CLOUDFLARE_D1_TOKEN ?? '',
+      },
+    });
+
+export default drizzle;
