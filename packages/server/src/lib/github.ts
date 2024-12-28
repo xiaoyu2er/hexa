de15ab9a529db4af6a5199f9f91f0c631e68f590
yanqi.zong <yzong@turo.com>
2024-12-28 09:44:05 -0800

    feat: move OAUTH_GITHUB from .dev.vars to .env

--- packages/server/src/lib/github.ts
+++ packages/server/src/lib/github.ts
@@ -2,5 +2,9 @@ import type { Context } from '@hexa/server/route/route-types';
 // @ts-ignore
 import { GitHub } from 'arctic';
 
-export const getGitHub = (env: Context['Bindings']) =>
-  new GitHub(env.OAUTH_GITHUB_CLIENT_ID, env.OAUTH_GITHUB_CLIENT_SECRET, null);
+export const getGitHub = (_env: Context['Bindings']) =>
+  new GitHub(
+    process.env.OAUTH_GITHUB_CLIENT_ID ?? '',
+    process.env.OAUTH_GITHUB_CLIENT_SECRET ?? '',
+    null
+  );
