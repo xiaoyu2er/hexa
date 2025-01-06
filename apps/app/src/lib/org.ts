1dd5c1f1ed5ad4e6dc2951cb77e97b07694117ad
yanqi.zong <yzong@turo.com>
2025-01-06 00:01:34 -0800

    feat: update avatar fallback url

--- apps/app/src/lib/org.ts
+++ apps/app/src/lib/org.ts
@@ -1,5 +1,10 @@
+import { icons } from '@dicebear/collection';
+import { createAvatar } from '@dicebear/core';
 import type { SelectOrgType } from '@hexa/server/schema/org';
 
 export const getOrgAvatarFallbackUrl = (org: SelectOrgType) => {
-  return `https://api.dicebear.com/9.x/icons/svg?seed=${org.name}`;
+  const avatar = createAvatar(icons, {
+    seed: org.id,
+  });
+  return avatar.toDataUri();
 };
